dcaec62b5cc3ab4824c6d079fea17c3a7ca673e4
ROBIN\Kurt <KurtA@Gjoll.com>
2020-12-17 15:02:17 -0600

    .

--- Projects/JavaDocParser/CSRewriter.cs
+++ Projects/JavaDocParser/CSRewriter.cs
@@ -1,10 +1,82 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
 
 namespace Eir.JavaDocParser
 {
-    class CSRewriter
+    public class CSRewriter : CSharpSyntaxRewriter
     {
+        private CSParser csParser;
+        
+        public CSRewriter(CSParser csParser)
+        {
+            this.csParser = csParser;
+        }
+
+        public override SyntaxNode? VisitInterfaceDeclaration(InterfaceDeclarationSyntax node)
+        {
+            return base.VisitInterfaceDeclaration(node);
+        }
+
+        //public override SyntaxNode VisitVariableDeclaration(VariableDeclarationSyntax node)
+        //{
+        //    // determination of the type of the variable(s)
+        //    var typeSymbol = (ITypeSymbol)this.SemanticModel.GetSymbolInfo(node.Type).Symbol;
+
+        //    bool changed = false;
+
+        //    // you could declare more than one variable with one expression
+        //    SeparatedSyntaxList<VariableDeclaratorSyntax> vs = node.Variables;
+        //    // we create a space to improve readability
+        //    SyntaxTrivia space = SyntaxFactory.SyntaxTrivia(SyntaxKind.WhitespaceTrivia, " ");
+
+        //    for (var i = 0; i < node.Variables.Count; i++)
+        //    {
+        //        // there is not an initialization
+        //        if (this.SemanticModel.GetSymbolInfo(node.Type).Symbol.ToString() == "int" &&
+        //            node.Variables[i].Initializer == null)
+        //        {
+        //            // we create a new espression "42"
+        //            // preceded by the space we create earlier
+        //            ExpressionSyntax es = SyntaxFactory.ParseExpression("42")
+        //                                               .WithLeadingTrivia(space);
+
+        //            // basically we create an assignment to the espression we just created
+        //            EqualsValueClauseSyntax evc = SyntaxFactory.EqualsValueClause(es)
+        //                                                       .WithLeadingTrivia(space);
+
+        //            // we replace the null initializer with ours
+        //            vs = vs.Replace(vs.ElementAt(i), vs.ElementAt(i).WithInitializer(evc));
+
+        //            changed = true;
+        //        }
+
+        //        // there is an initialization but it's not to 42
+        //        if (this.SemanticModel.GetSymbolInfo(node.Type).Symbol.ToString() == "int" &&
+        //            node.Variables[i].Initializer != null &&
+        //            !node.Variables[i].Initializer.Value.IsEquivalentTo(SyntaxFactory.ParseExpression("42")))
+        //        {
+        //            ExpressionSyntax es = SyntaxFactory.ParseExpression("42")
+        //                                               .WithLeadingTrivia(space);
+
+        //            EqualsValueClauseSyntax evc = SyntaxFactory.EqualsValueClause(es);
+
+        //            vs = vs.Replace(vs.ElementAt(i), vs.ElementAt(i).WithInitializer(evc));
+
+        //            changed = true;
+        //        }
+        //    }
+
+        //    if (changed == true)
+        //    {
+        //        return node.WithVariables(vs);
+        //    }
+
+        //    return base.VisitVariableDeclaration(node);
+        //}
     }
 }
